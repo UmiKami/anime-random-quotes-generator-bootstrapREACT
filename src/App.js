44e2aeeaf3891a2f618566b379aedc532707815f
umikami <ggoku02@gmail.com>
2022-02-26 17:42:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container-fluid main-content d-flex justify-content-center align-items-center">
+			<div className="card col-6">
+				<div className="card-body">
+					<blockquote className="blockquote mb-0">
+						<p>A well-known quote, contained in a blockquote element.</p>
+						<footer className="blockquote-footer">
+							Someone famous in <cite title="Source Title">Source Title</cite>
+						</footer>
+
+            <button className="btn btn-primary">New Quote</button>
+					</blockquote>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
