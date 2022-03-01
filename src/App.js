780ee956e0dffb07c60770bfbbde4d2f0992f3f7
umikami <ggoku02@gmail.com>
2022-03-01 12:30:44 -0500

    anime quote generator finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,69 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+	const [ hasLoaded, setHasLoaded ] = useState(false);
+
+	const [ quote, setQuote ] = useState('');
+	const [ character, setCharacter ] = useState('');
+	const [ title, setTitle ] = useState('');
+
+	useEffect(
+		() => {
+			if (!hasLoaded) {
+				setHasLoaded(true);
+
+				fetch('http://animechan.vercel.app/api/random', {
+					method: 'GET',
+					headers: {}
+				})
+					.then((response) => {
+						return response.json();
+					})
+					.then((result) => {
+						setQuote(result.quote);
+						setCharacter(result.character);
+						setTitle(result.anime);
+					})
+					.catch((err) => {
+						console.error(err);
+					});
+			}
+		},
+		[ quote, setHasLoaded, hasLoaded, title ]
+	);
+
+	const changeQuote = () => {
+		fetch('http://animechan.vercel.app/api/random', {
+			method: 'GET',
+			headers: {}
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((result) => {
+				setQuote(result.quote);
+				setCharacter(result.character);
+				setTitle(result.anime);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
+
 	return (
 		<div className="container-fluid main-content d-flex justify-content-center align-items-center">
-			<div className="card col-6">
+			<div className="card col-4">
 				<div className="card-body">
 					<blockquote className="blockquote mb-0">
-						<p>A well-known quote, contained in a blockquote element.</p>
+						<p className="quote py-2">"{quote}"</p>
 						<footer className="blockquote-footer">
-							Someone famous in <cite title="Source Title">Source Title</cite>
+							{character} in<cite title="Source Title"> {title}</cite>
 						</footer>
 
-            <button className="btn btn-primary">New Quote</button>
+						<button className="btn btn-primary-v2" onClick={() => changeQuote()}>
+							New Quote
+						</button>
 					</blockquote>
 				</div>
 			</div>
